6c03f03807f9bc94c82decec41893feb68b80773
joshua lescano <philipjoshua_lesano@yahoo.com>
2019-11-26 04:58:47 -0800

    v1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SignupComponent} from './signup/signup.component';
 import {HomeComponent} from './home/home.component';
-import {ItempageComponent} from './itempage/itempage.component';
 import {OrderlistComponent} from './orderlist/orderlist.component';
 import {UsermanagementComponent} from './usermanagement/usermanagement.component';
 import {ProductssComponent} from './productss/productss.component';
@@ -14,7 +13,6 @@ const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'signup', component: SignupComponent},
   {path:'home', component: HomeComponent},
-  {path:'itemPage/:id', component: ItempageComponent},
   {path:'orderlist', component: OrderlistComponent,  canActivate:[AuthGuard]},
   {path:'usermanagement', component: UsermanagementComponent,  canActivate:[AuthGuard]},
   {path:'products', component: ProductssComponent, canActivate:[AuthGuard]},
