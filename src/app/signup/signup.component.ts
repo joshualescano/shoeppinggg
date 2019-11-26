991ba554621fb0710d06bc4d77788b4474477ad2
joshua lescano <philipjoshua_lesano@yahoo.com>
2019-11-25 22:41:30 -0800

    God bless us

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {CustomerService} from '../customer.service';
 import { Customer } from '../customer';
-import  swal  from 'sweetalert';
+//import  swal  from 'sweetalert';
 import { Router } from '@angular/router';
 
 
@@ -47,7 +47,7 @@ export class SignupComponent {
     customer.phone = this.phone;
 
     if(this.password != this.confirm){
-      swal("Error!", "password not match!", "error");
+   //   swal("Error!", "password not match!", "error");
     }
     else{
       this.customerService.addCustomer(customer).subscribe(res =>{
@@ -56,7 +56,7 @@ export class SignupComponent {
       err => console.log(err)      
       ) 
 
-      swal("Congratulations!", "Welcome to shoepping family!", "success");
+//      swal("Congratulations!", "Welcome to shoepping family!", "success");
 
       this.username = "";
       this.password = "";
